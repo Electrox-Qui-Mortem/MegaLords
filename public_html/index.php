1aa113bb037b13fcd9bfba5885067eb12fa969df
Electrox-Qui-Mortem <Electrox-Qui-Mortem@users.noreply.github.com>
2018-04-27 12:04:30 -0400

    update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,218 +1,172 @@
 <!DOCTYPE html>
 <html>
-
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <title>MegaLords</title>
+        <link rel="stylesheet" href="styles/main.css">
         <style>
-            body {font-family: Arial;}
-            * {box-sizing: border-box}
-            /* Full-width input fields */
-            input[id = "usernameLogin"], input[id = "passwordLogin"] {
-                width: 100%;
-                padding: 12px 20px;
-                margin: 8px 0;
-                display: inline-block;
-                border: 1px solid #ccc;
-                box-sizing: border-box;
+            #fileToUpload {
+                display:none;
             }
-
-            input[id = "usernameSignUp"], input[id = "passwordSignUp"] {
-                width: 100%;
-                padding: 15px;
-                margin: 5px 0 22px 0;
-                display: inline-block;
-                border: none;
-                background: #f1f1f1;
-            }
-
-            /* Add a background color when the inputs get focus */
-            input[type=text]:focus, input[type=password]:focus {
-                background-color: #ddd;
-                outline: none;
-            }
-
-            /* Set a style for all buttons */
-            button[class = "signUpButton"] {
-                background-color: #4CAF50;
-                color: white;
-                padding: 14px 20px;
-                margin: 8px 0;
-                border: none;
-                cursor: pointer;
-                width: 100%;
-                opacity: 0.9;
-            }
-            button[class = "loginButton"] {
-                background-color: #4CAF50;
-                color: white;
-                padding: 14px 20px;
-                margin: 8px 0;
-                border: none;
-                cursor: pointer;
-                width: 100%;
-            }
-            button:hover {
-                opacity:1;
-            }
-            /* The Modal (background) */
-            .modal {
-                display: none; /* Hidden by default */
-                position: fixed; /* Stay in place */
-                z-index: 1; /* Sit on top */
-                left: 0;
-                top: 0;
-                width: 100%; /* Full width */
-                height: 100%; /* Full height */
-                overflow: auto; /* Enable scroll if needed */
-                background-color: #474e5d; /* Black w/ opacity */
-                padding-top: 60px;
-            }
-            /* Extra styles for the cancel button */
-            .cancelSignUpbtn {
-                padding: 14px 20px;
-                background-color: #f44336;
-                float: left;
-                width: 50%;
-                color: white;
-                padding: 14px 20px;
-                margin: 8px 0;
-                border: none;
-                cursor: pointer;
-                opacity: 0.9;
-                
-            }
-            .cancelLoginbtn {
-                width: auto;
-                padding: 10px 18px;
-                background-color: #f44336; 
-                color: white;
-                margin: 8px 0;
-                border: none;
+            #logout {
+                all: initial;
+                font-family: "Comic Sans MS", "Comic Sans", cursive;
                 cursor: pointer;
             }
-            /* Float cancel and signup buttons and add an equal width */
-            .signupbtn {
-                float: left;
-                width: 50%; 
-                background-color: #4CAF50;
-                color: white;
-                padding: 14px 20px;
-                margin: 8px 0;
-                border: none;
-                cursor: pointer;
-                opacity: 0.9;
-            }
-
-            /* Add padding to container elements */
-            .container {
-                padding: 16px;
-            }   
-
-            /* The Modal (background) */
-
-
-            /* Modal Content/Box */
-            .modal-content {
-                background-color: #fefefe;
-                margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
-                border: 1px solid #888;
-                width: 80%; /* Could be more or less, depending on screen size */
-            }
-            .imgcontainer {
-                text-align: center;
-                margin: 24px 0 12px 0;
-                position: relative;
-            }
-
-            img.avatar {
-            width: 40%;
-            border-radius: 50%;
-            }
-            /* Style the horizontal ruler */
-            hr {
-                border: 1px solid #f1f1f1;
-                margin-bottom: 25px;
-            }
- 
-            /* The Close Button (x) */
-            .close {
-                position: absolute;
-                right: 35px;
-                top: 15px;
-                font-size: 40px;
-                font-weight: bold;
-                color: #f1f1f1;
-            }
-
-            .close:hover,
-            .close:focus {
-            color: #f44336;
-            cursor: pointer;
-            }
-
-            /* Clear floats */
-            .clearfix::after {
-                content: "";
-                clear: both;
-                display: table;
-            }
-            span.psw {
-                float: right;
-                padding-top: 16px;
-            }
-            /* Change styles for cancel button and signup button on extra small screens */
-            @media screen and (max-width: 300px) {
-                .cancelbtn, .signupbtn {
-                    width: 100%;
-                }
+            #logout:hover{
+                color: blue;
             }
         </style>
+        <script src="script/main.js"></script>
+    </head>
     <body>
-        <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;" class = "signUpButton">Sign Up</button>
-<!---->
-        <button onclick="document.getElementById('id02').style.display='block'" style="width:auto;" class = "loginButton">Login</button>
+        <div id="test" class = "topnav">
+            <a class=active>Home</a>
+            <a href=news.php>News</a>
+            <a href=contact.php>Contact</a>
+            <a href=about.php>About</a>
+            <a href=buy.php>Buy</a>
+            <?php
+                if(isset($_COOKIE['username'])){
+                    echo    "<a href=game.php>Play</a>";
+                    echo    "<button onclick=" . "document.getElementById('id04').style.display='block'" . ">Profile</button>";
 
-        <div id="id02" class="modal">
-  
-  
-            <form class="modal-content animate" name="form1" method="post" action="checklogin.php">      
+                }else{
+                    echo "<button onclick=" . "document.getElementById('id01').style.display='block'" . ">Sign Up</button>";
+                    echo "<button onclick=" . "document.getElementById('id02').style.display='block'" . "> Login</button>";
+                }
+                ?>
+        </div>
+        <div id="id01" class="modal" style = 'padding-top: 50px;'>
+            <form id=signUpForm class="modal-content">
+                <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                <div class="container">
+                    <h1>Sign Up</h1>
+                    <p>Please fill in this form to create an account.</p>
+                    <hr>
+                    <p id ="errusra" class="err"></p>
+                    <label><b>Username</b></label>
+                    <input id="signUpUsername" type="text" placeholder="Enter Username" class = "profile" required>
+                    <p id="erremaila" class="err"></p>
+                    <label><b>Email</b></label>
+                    <input type="text" placeholder="Enter Email" id="signUpEmail" class = "profile" required>
+                    <p id="errpswa" class="err"></p>
+                    <label><b>Password</b></label>
+                    <input id="signUpPassword"type="password" placeholder="Enter Password" class = "profile" required>
+                    <p id="errrepswa" class="err"></p>
+                    <label><b>Repeat Password</b></label>
+                    <input type="password" placeholder="Repeat Password" id="passwordRepeat" class = "profile" required>
+                    <label>
+                        <input type="checkbox" checked="checked" style="margin-bottom:15px"> Remember me
+                    </label>
+                    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+                    <div class="clearfix">
+                        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelSignUpbtn">Cancel</button>
+                        <button type="submit" class="signupbtn">Sign Up</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div id="id02" class="modal" style = "padding-top: 50px;">
+            <form id="loginform"class="modal-content animate">      
                 <div class="imgcontainer">
                     <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
-                    <img src="/images/loginformimg.png" alt="Avatar" class="avatar">
+                    <img src="images/loginformimg.png" alt="Avatar" class="avatar">
                 </div>
                 <div class="container">
+                    <p id="loginerr" class = 'err'></p> 
                     <label><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name = "username" id = usernameLogin required>
-
+                    <input id="loginUsername"type="text" placeholder="Enter Username" class="profile" required>
                     <label><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="password" required id = "passwordLogin">
+                    <input id="loginPassword"type="password" placeholder="Enter Password" class = "profile" required>
                     <label>
                         <input type="checkbox" checked="checked" name = "cookies"> Remember me
                     </label>
-                    <button type="submit">Login</button>
-      
                 </div>
-
-                <div class="container" style="background-color:#f1f1f1">
+                <div class="container" style="background-color:#f1f1f1; overflow:auto;">
+                    <button type="submit"style="background-color: #4CAF50;
+                                                color: white;
+                                                padding: 14px 20px;
+                                                margin: 8px 0;
+                                                border: none;
+                                                cursor: pointer;
+                                                width: 100%;">Login</button>
                     <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelLoginbtn loginButton">Cancel</button>
                     <span class="psw">Forgot <a href="#">password?</a></span>
                 </div>
             </form>
         </div>
-        <!---->
-        <script>
-            // Get the modal
-            var modalLogin = document.getElementById('id01');
-            var modalSignUp = document.getElementById('id02');
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-                if (event.target == modalLogin) {
-                    modalLogin.style.display = "none";
-                }
-            }
-            window.onclick = function(event) {
-                if (event.target == modalSignUp) {
-                    modalSignUp.style.display = "none";
-                }
-            }
-        </script>
+        <div id="id04" class="modal" style = "padding-top: 50px;">
+            <form id="changeInfoForm"class = "modal-content animate">
+                <div class="imgcontainer">
+                    <?php
+                        if(isset($_COOKIE['profilepic'])){
+                            echo "<span onclick=" . '"document.getElementById(' . "'id04').style.display='none' " .  '" class="close" title="Close Modal">&times;</span>
+                                <label for="fileToUpload">
+                                    <img src="players/'. $_COOKIE['username']. '/' . $_COOKIE['profilepic'] . '" alt="Avatar" class="avatar" id="img">
+                                </label>
+                                <input type="file" name="fileToUpload" id="fileToUpload">';
+                        }else{
+                            echo "<span onclick=" . '"document.getElementById(' . "'id04').style.display='none' " .  '" class="close" title="Close Modal">&times;</span>
+                                <label for="fileToUpload">
+                                    <img src="images/loginformimg.png" alt="Avatar" class="avatar">
+                                </label>
+                                <input type="file" name="fileToUpload" id="fileToUpload">';
+                        }
+                    ?>
+                </div>
+                <div class="container">
+                    <label>
+                        <b>Username:</b> <input id=changeusername class="profile" name="username"value = <?php echo $_COOKIE['username'];?>>
+                    </label>
+                    <br>
+                    <label>
+                        <b>Email:</b> <input id=changeemail class="profile" name="email"value = <?php if(isset($_COOKIE['email'])){ echo $_COOKIE['email']; } ?>>
+                    </label>
+                    <br>
+                    <label>
+                        <b>New Password:</b> <input id=changepassword type="password" name="new-password"class="profile">
+                    </label>
+                    <br>
+                    <label>
+                        <b>Current Password:</b><input id="changerepassword"type="password" name="password"class="profile" required>
+                    </label>
+                    <br>
+                </div>
+                <div class="container" style="background-color:#f1f1f1; overflow:auto;">
+                    <input type="submit"style="background-color: #4CAF50;
+                                                color: white;
+                                                padding: 14px 20px;
+                                                margin: 8px 0;
+                                                border: none;
+                                                cursor: pointer;
+                                                width: 100%;" value="Change Profile Info"name="submit">
+                    <button type="button" onclick="document.getElementById('id04').style.display='none'" class="cancelLoginbtn loginButton">Cancel</button>
+                    <span class="psw"><a id="logout"onclick="logout()">Logout</a></span>
+                </div>
+            </form>
+        </div>
+        <script src="profile.js"></script>
+        <?php include 'selectimage.php'?>
+        <h1>Welcome to MegaLords</h1>
+        <p>MegaLords is a place where mortals can become gods, and fallen gods can become true gods once more. Becoming a god is a very difficult process, and cannot be explained. Contact me if you wish to become a god.
+            <h4>Requirements</h4>
+            <ul>
+                <li>You must have beaten the game</li>
+                <li>You must be a mortal or fallen god</li>
+                <li>You must be weaker than a lower god</li>
+                <li>You cannot be a mythical creature, but we are looking for mythical creatures</li>
+            </ul>
+            <h4>Instructions</h4>
+            <ol>
+                <li>Beat the game</li>
+                <li>The Owner will immediately contact you, if not contact them</li>
+                <li>The Owner will grant you goddhood</li>
+            </ol>
+        </p>
+        <footer>
+            <p>Posted by: Electrox</p>
+            <p>Contact information: <a href="mailto:quimortemking@gmail.com">QuiMortemKing@gmail.com</a>.</p>
+        </footer>
     </body>
 </html>
